1dbbf00cdbb19833936e50de382fcf3710310ebe
Alex Dergachev <alex@evolvingweb.ca>
2013-04-10 03:32:49 -0400

    Cleaned up comments in passenger_rvm::bluepill

--- site-cookbooks/passenger_rvm/recipes/bluepill.rb
+++ site-cookbooks/passenger_rvm/recipes/bluepill.rb
@@ -1,8 +1,5 @@
-# workaround for bluepill bug https://github.com/opscode-cookbooks/bluepill/pull/6
-
 Chef::Log.info("Installing workaround for https://github.com/opscode-cookbooks/bluepill/pull/6")
 
-# workaround for bluepill bug https://github.com/opscode-cookbooks/bluepill/pull/6
 node.set['bluepill']['bin'] = "/usr/local/rvm/wrappers/#{node['passenger_rvm']['global_gemset']}/bluepill"
 
 include_recipe "bluepill" # install this after RVM but before nginx::source
