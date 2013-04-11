dd7212f78975a01931155207d146bf2ff30ab325
Alex Dergachev <alex@evolvingweb.ca>
2013-04-11 11:30:39 -0400

    stopped including rvm::default; no longer necessary given rvm::system_install

--- site-cookbooks/passenger_rvm/recipes/rvm.rb
+++ site-cookbooks/passenger_rvm/recipes/rvm.rb
@@ -1,7 +1,10 @@
+include_recipe "rvm::vagrant" 
 include_recipe "rvm::system" # install the default ruby
 
-include_recipe "rvm::default" # required to avoid https://github.com/fnichol/chef-rvm/issues/141
-include_recipe "rvm::vagrant" 
+# only required if not running rvm::system or rvm::system_install 
+# required to avoid https://github.com/fnichol/chef-rvm/issues/141
+## include_recipe "rvm::default" 
+
 include_recipe "rvm::gem_package" # makes gem_package resource respect RVM
 
 # create the default gemset for the application
