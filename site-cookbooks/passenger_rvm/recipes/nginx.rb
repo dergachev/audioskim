91a3c9525463391062aa681ada4779d2d9f4d661
Alex Dergachev <alex@evolvingweb.ca>
2013-04-11 11:42:54 -0400

    Added comments to passenger_rvm::nginx listing error msg

--- site-cookbooks/passenger_rvm/recipes/nginx.rb
+++ site-cookbooks/passenger_rvm/recipes/nginx.rb
@@ -5,7 +5,9 @@ raise "TRYING TO INSTALL NGINX FROM PACKAGE" unless node['nginx']['install_metho
 
 # workaround to make nginx::passenger compatible with RVM
 # adapted from https://github.com/substantial/cookbook-nginx/blob/master/recipes/rvm_passenger.rb#L51
-# otherwise nginx compilation will trigger passenger compilation, without rvmsudo
+# otherwise nginx compilation will trigger passenger compilation, without rvmsudo, and errors:
+#   *** Running 'rake nginx RELEASE=yes' in /usr/local/rvm/gems/ruby-1.8.7-p330/gems/passenger-3.0.19/ext/nginx... ***
+#   STDERR: /usr/local/rvm/rubies/ruby-1.8.7-p330/lib/ruby/site_ruby/1.8/rubygems/dependency.rb:247:in `to_specs': Could not find rake (>= 0) amongst [] (Gem::LoadError)
 rvm_shell "compile_passenger_support_files" do
   action :nothing
   subscribes :run, resources('gem_package[passenger]'), :immediately
